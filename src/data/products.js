2569572453b9d3e782d901a4a893fbe30c555fb6
ganeshramanan <49465676+ganeshramanan@users.noreply.github.com>
2026-05-07 16:19:15 +0530

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -3,18 +3,91 @@ export const products = [
     id: 1,
     name: "Milk",
     price: 30,
-    image: "https://images.unsplash.com/photo-1563636619-e9143da7973b?w=400",
+    image:
+      "https://images.unsplash.com/photo-1563636619-e9143da7973b?w=400",
   },
   {
     id: 2,
     name: "Bread",
     price: 25,
-    image: "https://images.unsplash.com/photo-1509440159596-0249088772ff?w=400",
+    image:
+      "https://images.unsplash.com/photo-1509440159596-0249088772ff?w=400",
   },
   {
     id: 3,
     name: "Eggs",
     price: 60,
-    image: "https://images.unsplash.com/photo-1582722872445-44dc5f7e3c8f?w=400",
+    image:
+      "https://images.unsplash.com/photo-1582722872445-44dc5f7e3c8f?w=400",
+  },
+  {
+    id: 4,
+    name: "Rice",
+    price: 80,
+    image:
+      "https://images.unsplash.com/photo-1586201375761-83865001e31c?w=400",
+  },
+  {
+    id: 5,
+    name: "Tomatoes",
+    price: 40,
+    image:
+      "https://images.unsplash.com/photo-1546094096-0df4bcaaa337?w=400",
+  },
+  {
+    id: 6,
+    name: "Potatoes",
+    price: 35,
+    image:
+      "https://images.unsplash.com/photo-1518977676601-b53f82aba655?w=400",
+  },
+  {
+    id: 7,
+    name: "Bananas",
+    price: 50,
+    image:
+      "https://images.unsplash.com/photo-1574226516831-e1dff420e37f?w=400",
+  },
+  {
+    id: 8,
+    name: "Apples",
+    price: 120,
+    image:
+      "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?w=400",
+  },
+  {
+    id: 9,
+    name: "Orange Juice",
+    price: 90,
+    image:
+      "https://images.unsplash.com/photo-1600271886742-f049cd451bba?w=400",
+  },
+  {
+    id: 10,
+    name: "Coffee",
+    price: 150,
+    image:
+      "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=400",
+  },
+  {
+    id: 11,
+    name: "Tea Powder",
+    price: 110,
+    image:
+      "https://images.unsplash.com/photo-1544787219-7f47ccb76574?w=400",
+  },
+  {
+    id: 12,
+    name: "Sugar",
+    price: 45,
+    image:
+      "https://images.unsplash.com/photo-1581441363689-1f3c3c414635?w=400",
+  },
+  {
+    id: 13,
+    name: "Cooking Oil",
+    price: 180,
+    image:
+      "https://images.unsplash.com/photo-1474979266404-7eaacbcd87c5?w=400",
   },
 ];
