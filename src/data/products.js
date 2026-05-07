17501cfc20723da748f469769608b8dcd86afa1c
unknown <tganeshramanan85@gmail.com>
2026-05-07 16:36:23 +0530

    Add more products

--- src/data/products.js
+++ src/data/products.js
@@ -90,4 +90,64 @@ export const products = [
     image:
       "https://images.unsplash.com/photo-1474979266404-7eaacbcd87c5?w=400",
   },
+  {
+    id: 4,
+    name: "Rice",
+    price: 80,
+    image: "https://images.unsplash.com/photo-1586201375761-83865001e31c?w=400",
+  },
+  {
+    id: 5,
+    name: "Tomatoes",
+    price: 40,
+    image: "https://images.unsplash.com/photo-1546094096-0df4bcaaa337?w=400",
+  },
+  {
+    id: 6,
+    name: "Potatoes",
+    price: 35,
+    image: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?w=400",
+  },
+  {
+    id: 7,
+    name: "Bananas",
+    price: 50,
+    image: "https://images.unsplash.com/photo-1574226516831-e1dff420e37f?w=400",
+  },
+  {
+    id: 8,
+    name: "Apples",
+    price: 120,
+    image: "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?w=400",
+  },
+  {
+    id: 9,
+    name: "Orange Juice",
+    price: 90,
+    image: "https://images.unsplash.com/photo-1600271886742-f049cd451bba?w=400",
+  },
+  {
+    id: 10,
+    name: "Coffee",
+    price: 150,
+    image: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=400",
+  },
+  {
+    id: 11,
+    name: "Tea Powder",
+    price: 110,
+    image: "https://images.unsplash.com/photo-1544787219-7f47ccb76574?w=400",
+  },
+  {
+    id: 12,
+    name: "Sugar",
+    price: 45,
+    image: "https://images.unsplash.com/photo-1581441363689-1f3c3c414635?w=400",
+  },
+  {
+    id: 13,
+    name: "Cooking Oil",
+    price: 180,
+    image: "https://images.unsplash.com/photo-1474979266404-7eaacbcd87c5?w=400",
+  },
 ];
